5ed725609d7b3db1f7492bf568f58b77a4bc4572
Ryan Philipps <raphilip@mtu.edu>
2018-10-25 21:21:41 -0400

    Made some view tests, so we can tell if we break stuff

--- spec/views/wiki_page/list.html.erb_spec.rb
+++ spec/views/wiki_page/list.html.erb_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe "wiki_page/index.html.erb", type: :view do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'No wikis in the database' do
+    before do
+      assign(:pages, []) # empty wiki
+      render
+    end
+    it 'should show a no wiki page message' do
+      expect(rendered).to include 'No Wiki pages on the site :('
+    end
+  end
 end
--- spec/views/wiki_page/show.html.erb_spec.rb
+++ spec/views/wiki_page/show.html.erb_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe "wiki_page/show.html.erb", type: :view do
-  pending "add some examples to (or delete) #{__FILE__}"
+  fake_page = nil
+  before do
+    fake_page = WikiPage.new(title: 'Title', content: 'This is the content')
+    fake_page.save # need to store it the db so it has some methods defined
+    assign(:page, fake_page)
+    render
+  end
+  it 'should show the title and content' do
+    expect(rendered).to include('Title')
+    expect(rendered).to include('This is the content')
+  end
+
+  after do
+    fake_page.destroy # get it out the database
+  end
 end
