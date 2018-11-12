5aa162becb5b7890821e252483c8ec13bbe84c2b
rjavadek <rjavadek@mtu.edu>
2018-11-12 02:55:56 -0500

    Successfully blocking all the IPs that are listed in config/blacklist. Tested with 127.0.0.1.

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -1,6 +1,5 @@
 # For blocking the users in the list 
 Rack::Attack.blocklist("Block users whose IP comes from the restricted IP LIST") do |request|
        	# Requests are blocked if the return value is truthy
-	# Put in the list here in place of demoBannedIPList
-	# Rails.configuration.demoBannedIPList.include?(request.ip)
+	File.readlines(Rails.root.join('config/blacklist')).grep(/#{Regexp.quote(request.ip)}/).any?
 end
