0165ed2fc82d22ff27884b8685786c7161123b03
rjavadek <rjavadek@mtu.edu>
2018-11-26 22:32:01 -0500

    Blocked IP users now see a text line, instead of just 'Forbidden'

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -1,4 +1,7 @@
 # For blocking the users in the list 
+
+
+Rack::Attack.blocklisted_response = lambda{ |_env| [ 403, { "Content-Type" => "text/plain" }, [ "You have been blocked from the system. If you think this has been done in error, please contact Admin. Thank you." ] ]} 
 Rack::Attack.blocklist("Block users whose IP comes from the restricted IP LIST") do |request|
        	# Requests are blocked if the return value is truthy
 	File.readlines(Rails.root.join('config/blacklist')).grep(/#{Regexp.quote(request.ip)}/).any?
